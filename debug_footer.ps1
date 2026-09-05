4c018499fc7c48cf0b4a83f9164c0877a8aa7d5f
Kiamapatrick <patokiama0420@gmail.com>
2026-09-05 09:20:30 +0300

    Remove 26 unused files: debug scripts, unused images, dead JS, editor config

--- debug_footer.ps1
+++
@@ -1,5 +0,0 @@
-$content = Get-Content 'C:\Users\ADMIN\Downloads\All-work\new work\apsis-new\index.html' -Raw
-$idx = $content.IndexOf('footer-links-col')
-if ($idx -gt -1) {
-    Write-Host $content.Substring($idx, 1000)
-}
